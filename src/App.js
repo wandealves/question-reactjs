9b78a8aca249845e2887f761b08b21da8adf79fa
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-07-11 11:30:23 -0300

    definição das rotas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Configuration from "./components/Configuration";
+import Questions from "./components/Questions";
+
 function App() {
   return (
     <div>
-
-      <div className="flex space-x-4">
-        <div className="flex-1 bg-blue-700 m-2 p-5">1</div>
-        <div className="flex-1 bg-pink-700 m-2 p-5">2</div>
-        <div className="flex-1 bg-red-700  m-2 p-5">3</div>
-      </div>
-
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/configurations">
+            <Configuration />
+          </Route>
+          <Route path="/questions">
+            <Questions />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./styles/tailwind.css";
 import App from "./App";
-import Header from "./components/Header";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
